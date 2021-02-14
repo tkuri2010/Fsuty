3c8889130c6e5e97b3401ec977e9578ad65410b7
tkuri2010 <tkuri2010@gmail.com>
2021-02-14 11:03:52 +0900

    more test impl

--- src/Tkuri2010.Fsuty.Tests/FsentryTest.cs
+++ src/Tkuri2010.Fsuty.Tests/FsentryTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,14 +9,25 @@ namespace Tkuri2010.Fsuty.Tests
 	[TestClass]
 	public class FsentryTest
 	{
-
 		static PathItems _AsItems(string relPath) => Filepath.Parse(relPath).Items;
 
+		static int tempDirSeq = 0;
 
-		private Filepath GetTempPath()
+		static int NextTempDirSeq() => ++tempDirSeq;
+
+		private static Filepath GetTempDir() =>
+			Filepath.Parse(System.IO.Path.GetTempPath())
+				.Combine(_AsItems("FsentryTestDir_" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + "_" + NextTempDirSeq()))
+				.Canonicalize()
+				.Also(self => {   // 安全そうなパスであることをできるだけチェック
+					Assert.IsTrue(self.IsAbsolute);
+					Assert.IsTrue(2 <= self.Items.Count);
+				});
+
+
+		private static void CleanupTempDir(Filepath tempDir)
 		{
-			var systemTemp = Filepath.Parse(System.IO.Path.GetTempPath());
-			return systemTemp.Combine(_AsItems("FsentryTestDir_" + DateTime.Now.ToString("yyyyMMdd-HHmmss")));
+			Directory.Delete(tempDir.ToString(), recursive: true);
 		}
 
 
@@ -37,8 +49,9 @@ namespace Tkuri2010.Fsuty.Tests
 		public async Task Test1Async()
 		{
 			// ディレクトリやファイルを実際のファイルシステム上に用意
-			var temp = GetTempPath();
-			var paths = new []
+			var temp = GetTempDir();
+			using var deleteLater = new Defer(() => CleanupTempDir(temp));
+			var pathSet = new HashSet<string>()
 			{
 				"file0.txt",
 				"dir1/file1-1.txt",
@@ -47,10 +60,8 @@ namespace Tkuri2010.Fsuty.Tests
 				"dir3-1/dir3-2/file3-1.txt",
 				"dir3-1/dir3-2/dir3-3/file3-2.txt",
 			};
-			var pathSet = new HashSet<string>();
-			foreach (var path in paths)
+			foreach (var path in pathSet)
 			{
-				pathSet.Add(path);
 				await SetupPathAsync(temp.Combine(_AsItems(path)));
 			}
 
@@ -58,23 +69,131 @@ namespace Tkuri2010.Fsuty.Tests
 			await foreach (var entry in Fsentry.VisitAsync(temp))
 			{
 				var fileName = System.IO.Path.GetFileName(entry.FullPathString);
-				var relPath = entry.RelativeParent.Combine(_AsItems(fileName)).ToString("/");
+				var relPathStr = entry.RelativeParent.Combine(_AsItems(fileName)).ToString("/");
+
 				if (entry.Event == FsentryEvent.EnterDir)
 				{
-					if (pathSet.Contains(relPath))
+					if (pathSet.Contains(relPathStr))
 					{
-						pathSet.Remove(relPath);
+						pathSet.Remove(relPathStr);
 					}
 				}
 				else if (entry.Event == FsentryEvent.File)
 				{
-					Assert.IsTrue(pathSet.Contains(relPath));
-					pathSet.Remove(relPath);
+					Assert.IsTrue(pathSet.Contains(relPathStr));
+					pathSet.Remove(relPathStr);
 				}
 			}
 
 			// pathSet は全て無くなったはず
 			Assert.AreEqual(0, pathSet.Count);
+		}
+
+
+		[TestMethod]
+		public async Task Test2Async()
+		{
+			// ディレクトリやファイルを実際のファイルシステム上に用意
+			var temp = GetTempDir();
+			using var deleteLater = new Defer(() => CleanupTempDir(temp));
+			var pathSet = new HashSet<string>()
+			{
+				"file0.txt",
+				"dir1/file1-1.txt",
+				"dir1/file1-2.txt",
+				"dir2",
+				"dir3-1/dir3-2/file3-1.txt",
+				"dir3-1/dir3-2/dir3-3/file3-2.txt",
+			};
+			foreach (var path in pathSet)
+			{
+				await SetupPathAsync(temp.Combine(_AsItems(path)));
+			}
+
+			// テスト実行。見つかったディレクトリやファイルを変数 pathSet から消していく
+			// ただし、dir3-1 の中身を無視してみる
+			var skipCount = 0;
+			await foreach (var entry in Fsentry.VisitAsync(temp))
+			{
+				var fileName = System.IO.Path.GetFileName(entry.FullPathString);
+				var relPathStr = entry.RelativeParent.Combine(_AsItems(fileName)).ToString("/");
+
+				if (entry.Event == FsentryEvent.EnterDir)
+				{
+					if (relPathStr.Contains("dir3-1"))
+					{
+						entry.Command = FsentryCommand.SkipDirectory;
+						skipCount++;
+						continue;
+					}
+
+					if (pathSet.Contains(relPathStr))
+					{
+						pathSet.Remove(relPathStr);
+					}
+				}
+				else if (entry.Event == FsentryEvent.File)
+				{
+					Assert.IsTrue(pathSet.Contains(relPathStr));
+					pathSet.Remove(relPathStr);
+				}
+			}
+
+			// 1度だけ skip したはず。"dir3-1" を見つけてすぐ skip したので、中のディレクトリの探索はしていないはず
+			Assert.AreEqual(1, skipCount);
+
+			// pathSet は 2 つ残ったはず
+			Assert.AreEqual(2, pathSet.Count);
+		}
+	}
+
+
+	/// <summary>
+	/// experimental... is this useful?
+	/// </summary>
+	static class Ext
+	{
+		/// <summary> (like Kotlin) </summary>
+		public static TSelf Also<TSelf>(this TSelf self, Action<TSelf> action)
+		{
+			action(self);
+			return self;
+		}
+	}
+
+
+	class Defer : IDisposable
+	{
+
+		Action DeferredAction { get; init; }
+
+
+		internal Defer(Action action)
+		{
+			DeferredAction = action;
+		}
+
+
+		private bool disposedValue;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					DeferredAction();
+				}
+
+				disposedValue = true;
+			}
+		}
+
+		public void Dispose()
+		{
+			// このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
 		}
 	}
 }
